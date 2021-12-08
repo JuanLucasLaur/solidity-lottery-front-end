fd0f8dd40bc5956ea980536196089dce727b578a
Juan L <33398520+JuanLucasLaur@users.noreply.github.com>
2021-12-08 17:14:07 -0300

    Add entry form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import './App.css';
+import React, {FormEventHandler, useEffect, useState} from 'react';
 import web3 from './web3';
 import lottery from './lottery';
 
+interface Status {
+    message: string;
+    ready: boolean;
+}
+
 function App() {
     const [manager, setManager] = useState('');
-    const [players, setPlayers] = useState(['']);
+    const [players, setPlayers] = useState<Array<string>>([]);
     const [balance, setBalance] = useState('');
+    const [entryFee, setEntryFee] = useState('');
+    const [status, setStatus] = useState<Status>({
+        message: 'Ready',
+        ready: true
+    });
 
     useEffect(() => {
         const fetchManager = async () => {
@@ -21,13 +32,51 @@ function App() {
         fetchManager();
     }, []);
 
+    const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+        event.preventDefault();
+
+        const accounts = await web3.eth.getAccounts();
+
+        setStatus({message: 'Waiting on transaction success...', ready: false});
+
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei(entryFee, 'ether')
+        })
+
+        setStatus({message: 'Entry successful', ready: true});
+    };
+
     return (
-        <div>
-            <h2>Lottery Contract</h2>
-            <p>Contract managed by {manager}</p>
-            <p>There are currently {players.length} people entered.</p>
-            <p>The current prize is of {web3.utils.fromWei(balance, 'ether')} ether.</p>
-        </div>
+        <>
+            <section>
+                <h2>Lottery Contract</h2>
+                <p>Contract managed by {manager}</p>
+                <p>There are currently {players.length} people entered.</p>
+                <p>The current prize is of {web3.utils.fromWei(balance, 'ether')} eth.</p>
+            </section>
+            <hr/>
+            <section>
+                <form onSubmit={handleSubmit}>
+                    <h4>Enter lottery</h4>
+                    {status.ready ? (<div className="entry-form__input">
+                        <label>Amount of eth to enter</label>
+                        <input
+                            min="0.00100001"
+                            onChange={event => {
+                                setEntryFee(event.target.value);
+                            }}
+                            required
+                            step="0.00000001"
+                            type={'number'}
+                            value={entryFee}
+                        />
+                        <button>Enter</button>
+                    </div>) : null}
+                    <strong>{status.message}</strong>
+                </form>
+            </section>
+        </>
     );
 }
 
